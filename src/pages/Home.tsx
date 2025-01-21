a3c7332b9b79ba691a6d1d6d0847df4c769ff544
Matthew McNee <matt@acyleris.com>
2025-01-21 10:53:23 +0000

    Add rules and information to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import GameCard from "@components/ui/GameCard";
 import Navbar from "@components/ui/Navbar";
 import { useAuth } from "@firebase/useAuth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ClickableText } from "@components/ui/Buttons";
 
 
@@ -28,12 +28,37 @@ const Home = () => {
         </>
         )}
 
-        <br></br>
+        <br/>
 
         <div className="cards-container">
           <GameCard type="mango" />
           <GameCard type="beans" />
         </div>
+
+        <br/>
+        <br/>
+
+        <h2>Mango rules</h2>
+
+        <p>To finish the game, every square must contain either a mango or a banana.</p>
+        <p>There must be the same number of mangoes and bananas in each row and column.</p>
+        <p>No more than 2 mangoes or bananas may be next to each other, either vertically or horizontally.</p>
+        <p>Cells separated by an = sign must be of the same type and cells separated by an x sign must be of the opposite type.</p>
+        <p>There may be more than one valid answer and the game will accept any valid solution.</p>
+
+        <h2>Beans rules</h2>
+        <p>To finish the game, ten beans must be placed, each in their own row, column and colour.</p>
+        <p>Beans cannot be directly next to other beans, either vertically, horizontally or diagonally.</p>
+        <p>Places where a bean cannot be placed may be marked with and x.</p>
+        <p>There may be more than one valid answer and the game will accept any valid solution.</p>
+
+        <h2>About This Application</h2>
+        <p>This is an open source project that accepts community contributions. The code is available <Link to="https://github.com/mattmcnee/fruits-and-squares">here</Link>.</p>
+        <p>Fruits & Squares is an independent product and is not affiliated with, nor has it been authorized, sponsored, or otherwise approved by LinkedIn Corporation.</p>
+        <p>Users are encouraged to play the games these puzzles are inspired by, namely <Link to="https://www.linkedin.com/showcase/tango-game/">Tango</Link> and <Link to="https://www.linkedin.com/showcase/queens-game/">Queens</Link>.</p>
+
+
+
       </div>
     </div>
   );
